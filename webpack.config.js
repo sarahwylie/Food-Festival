a4fd39d643a3040b9d1f103c41eba29be6477014
sarahwylie <sarahannemariawylie@gmail.com>
2022-03-15 15:59:53 -0500

    ready for PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack")
 const path = require("path");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-module.exports = {
+const config = {
   entry: {
       app: "./assets/js/script.js",
       events: "./assets/js/events.js",
@@ -44,7 +45,24 @@ module.exports = {
   }),
   new BundleAnalyzerPlugin({
     analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+  }),
+  new WebpackPwaManifest({
+    name: "Food Fest",
+    short_name: "Foodies",
+    description: "An app that allows you to navigate upcoming food events",
+    start_url: "../index.html",
+    background_color: "#01579b",
+    theme_color: "#ffffff",
+    fingerprints: false,
+    inject: false,
+    icons: [{
+      src: path.resolve("assets/img/icons/icon-512x512.png"),
+      sizes: [96, 128, 192, 256, 384, 512],
+      destination: path.join("assets", "icons")
+    }]
   })
   ],
   mode: "development"
 };
+
+module.exports = config;
